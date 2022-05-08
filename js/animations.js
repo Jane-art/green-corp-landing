43642b1973f7aecd6cd5e3027c327136363418f5
Jane-art <52426617+Jane-art@users.noreply.github.com>
2022-05-08 23:04:32 +0300

    добавляем поле через вызов события

--- js/animations.js
+++ js/animations.js
@@ -23,3 +23,31 @@ function initIncreaseNumberAnimation() {
 }
 
 initIncreaseNumberAnimation();
+document.querySelector("#budget").addEventListener("change", function handleSelectChange(event) {
+  
+    if (event.target.value === 'other') {
+    // console.log("Пытаемся добавить текстовое поле");
+    
+    const input = document.createElement('input');
+    input.placeholder = "Введите ваш вариант";
+    input.type = "text";
+
+    const formContainer = document.createElement('div');
+    formContainer.classList.add('form__group');
+    formContainer.classList.add('form__other-input');
+    formContainer.id = 'form__other-input';
+    formContainer.appendChild(input);
+
+    let root = document.querySelector('form');
+    
+    let button = document.getElementsByClassName('form__submit');
+    
+    root.insertBefore(formContainer, button[0]);
+    
+  }
+
+  if (event.target.value !== 'other') {
+    document.getElementById('form__other-input').remove();
+
+  }
+});
