783d44956cf02dcd1e52697ed718e8261c74d705
Jane-art <52426617+Jane-art@users.noreply.github.com>
2022-05-09 16:07:23 +0300

    завершение работы над проектом

--- js/animations.js
+++ js/animations.js
@@ -22,7 +22,7 @@ function initIncreaseNumberAnimation() {
   increaseNumberAnimationStep(0, element, 5000);
 }
 
-initIncreaseNumberAnimation();
+
 document.querySelector("#budget").addEventListener("change", function handleSelectChange(event) {
   
     if (event.target.value === 'other') {
@@ -51,3 +51,38 @@ document.querySelector("#budget").addEventListener("change", function handleSele
 
   }
 });
+
+
+function addSmoothScroll(anchor) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+ 
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+}
+ 
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  addSmoothScroll(anchor);
+});
+
+addSmoothScroll(document.querySelector('.more-button'));
+
+function updateScroll() {
+  if (window.scrollY > 0) {
+    document.querySelector('header').classList.add('header__scrolled');
+  } else {
+    document.querySelector('header').classList.remove('header__scrolled');
+  }
+ 
+  // Запуск анимации увеличения числа
+  let windowBottomPosition = window.scrollY + window.innerHeight;
+  let countElementPosition = document.querySelector('.features__clients-count').offsetTop;
+  if (windowBottomPosition >= countElementPosition && !animationInited) {
+    animationInited = true;
+    
+  }
+}
+initIncreaseNumberAnimation();
+window.addEventListener('scroll', updateScroll);
